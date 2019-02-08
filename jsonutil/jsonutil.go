84de3f01e6b9d1565145f9b08c4e6c0610e7e6e3
Alexander <lexmail@gmail.com>
2019-02-08 12:42:44 +0100

    AtoiWithLogging, renames

--- jsonutil/jsonutil.go
+++ jsonutil/jsonutil.go
@@ -1,4 +1,4 @@
-package jsonhelper
+package jsonutil
 
 import (
 	"encoding/json"
---
+++ stringutil/stringutil.go
@@ -0,0 +1,21 @@
+package stringutil
+
+import (
+	"strconv"
+
+	"github.com/alexandervantrijffel/goutil/logging"
+)
+
+func AtoiWithLogging(input string) int {
+	var iDefault int
+	if len(input) == 0 {
+		logging.Warning("Converting empty value to int. Setting it to default value 0.")
+		return iDefault
+	}
+	if i, err := strconv.Atoi(input); err == nil {
+		return i
+	} else {
+		logging.Errorf("Failed to convert value %s to int", input)
+		return iDefault
+	}
+}
