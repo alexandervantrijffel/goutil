abdc8455f67c51c433462d81b2688a9e965d1e09
Alexander <lexmail@gmail.com>
2019-02-19 21:06:16 +0100

    MarshalWithLogging json helper

--- jsonutil/jsonutil.go
+++ jsonutil/jsonutil.go
@@ -6,6 +6,11 @@ import (
 	"github.com/alexandervantrijffel/goutil/errorcheck"
 )
 
+func MarshalWithLogging(source interface{}) []byte {
+	bytes, err := json.Marshal(source)
+	_ = errorcheck.CheckLogf(err, "Failed to marshal data to type %T. %+v", source, source)
+	return bytes
+}
 func UnmarshalWithLogging(destination interface{}, source []byte) error {
 	err := json.Unmarshal(source, destination)
 	_ = errorcheck.CheckLogf(err, "Failed to unmarshal data to type %T. (start of) data: %s", destination, firstChars(string(source), 512))
